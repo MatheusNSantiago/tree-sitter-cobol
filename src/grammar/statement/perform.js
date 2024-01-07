c1a4653908a1e96a7b603c5a3fea1ff26ead68d5
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-01-06 23:17:03 -0300

    feat(statement/perform): add perform until

--- src/grammar/statement/perform.js
+++ src/grammar/statement/perform.js
@@ -1,11 +1,29 @@
 module.exports = {
-  perform_statement: ($) => choice($.perform_simple),
+  perform_statement: ($) => choice($.perform_simple, $.perform_until),
 
+  _PERFORM: (_) => kw("PERFORM"),
+  _END_PERFORM: (_) => kw("END-PERFORM"),
+
+  // ╾───────────────────────────────────────────────────────────────────────────────────╼
+  //  PERFORM 100000-FOO.
+  // ╾───────────────────────────────────────────────────────────────────────────────────╼
   perform_simple: ($) =>
     seq(
-      kw("PERFORM"),
+      $._PERFORM,
       field("label", $.section_name),
       optional(seq($._THRU, $.section_name)),
       optional("."),
+    ),
+
+  // ╾───────────────────────────────────────────────────────────────────────────────────╼
+  //  PERFORM UNTIL IND-FIM-610E = 1
+  //       DISPLAY "FOO"
+  //  END-PERFORM.
+  // ╾───────────────────────────────────────────────────────────────────────────────────╼
+  perform_until: ($) =>
+    seq(
+      seq($._PERFORM, kw("UNTIL"), $.expr),
+      repeat($._statement),
+      seq($._END_PERFORM, optional(".")),
     ),
 };
