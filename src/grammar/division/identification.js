0023811db5f2a3d1ebc77acf6223727a0c324054
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-12-23 00:35:37 -0300

    fix(identification): faz o '.' no final do nome do author opcional

--- src/grammar/division/identification.js
+++ src/grammar/division/identification.js
@@ -11,9 +11,9 @@ module.exports = {
   identification_division_header: (_) =>
     seq(kw("IDENTIFICATION"), kw("DIVISION"), "."),
 
-  program_id: ($) => seq(kw("PROGRAM-ID"), ".", $.program_name, "."),
+  program_id: ($) => seq(kw("PROGRAM-ID"), ".", $.program_name, op(".")),
   program_name: ($) => $._WORD,
 
-  author: ($) => seq(C($), kw("AUTHOR"), ".", $.author_name, "."),
-  author_name: ($) => $._WORD,
+  author: ($) => seq(C($), kw("AUTHOR"), ".", $.author_name, op(".")),
+  author_name: ($) => repeat1($._WORD),
 };
