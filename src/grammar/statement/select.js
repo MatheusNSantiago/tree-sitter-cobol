96b3fbf32249a236176e1f41ae2a5e39710d8c42
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-12-26 20:55:21 -0300

    feat: add fields to select statement

--- src/grammar/statement/select.js
+++ src/grammar/statement/select.js
@@ -2,10 +2,10 @@ module.exports = {
   select_statement: ($) =>
     seq(
       kw("SELECT"),
-      $.file_name,
+      field("file", $.file_name),
       seq($._select_clause, "."), //
     ),
-  _select_clause: ($) => choice($.assign_clause),
+  _select_clause: ($) => choice($._assign_clause),
 
-  assign_clause: ($) => seq(kw("ASSIGN"), $._TO, $._WORD),
+  _assign_clause: ($) => seq(kw("ASSIGN"), $._TO, field("dd_name", $.WORD)),
 };
